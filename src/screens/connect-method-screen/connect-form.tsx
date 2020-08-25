e7ab20f544598c45f3ddc29320e4964d390bdcc2
Pierce Harmon <pierceh.dev@gmail.com>
2020-08-25 15:49:35 -0500

    W.I.P.

--- src/screens/connect-method-screen/connect-form.tsx
+++ src/screens/connect-method-screen/connect-form.tsx
@@ -1,4 +1,5 @@
 // Third Party
+import { Modal } from 'antd';
 import React, { FunctionComponent } from 'react';
 import { withTheme, FormSubmit } from '@rjsf/core';
 import { Theme as AntDTheme } from '@rjsf/antd';
@@ -6,19 +7,40 @@ import { JSONSchema7 } from 'json-schema';
 import { isEqual } from 'lodash';
 import axios from 'axios';
 
+// Components
+import Spinner from '../../components/spinner';
+
 const Form = withTheme(AntDTheme);
 
 interface Props {
   schema: JSONSchema7;
 }
-
-const handleSubmit = async (formSubmit: FormSubmit) => {
-  console.log(formSubmit.formData);
-  await axios.put('http://localhost:3000/connect', formSubmit.formData);
-};
-
 const ConnectFrom: FunctionComponent<Props> = ({ schema }) => {
-  return <Form schema={schema} onSubmit={handleSubmit} />;
+  const [modalVisible, setModalVisible] = React.useState(true);
+
+  const handleSubmit = async (formSubmit: FormSubmit) => {
+    setModalVisible(true);
+    await axios.put('http://localhost:3000/connect', formSubmit.formData);
+  };
+
+  return (
+    <>
+      <Modal
+        title='Request Logs'
+        footer={null}
+        width={900}
+        visible={modalVisible}
+        closable={true}
+        onCancel={() => {
+          setModalVisible(false);
+        }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Spinner />
+        </div>
+      </Modal>
+      <Form schema={schema} onSubmit={handleSubmit} />
+    </>
+  );
 };
 
 export default React.memo(ConnectFrom, function compare(
