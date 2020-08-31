f78833e7f29f82d05a8b3851c605705b8c34b1c6
Pierce Harmon <pierceh.dev@gmail.com>
2020-08-31 11:42:05 -0500

    W.I.P.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const apiHost = process.env.REACT_APP_API_HOST!;
+import env from '@beam-australia/react-env';
+
+let apiHost;
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  apiHost = process.env.REACT_APP_API_HOST!;
+} else {
+  apiHost = env('API_HOST');
+}
 
 const API = {
   getAppInfo: apiHost,
